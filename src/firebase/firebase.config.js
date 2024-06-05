c293c5999cbb63cf6147da8ab58f70e0ef79df8c
5anne <tahaminaanne@gmail.com>
2024-06-05 23:32:09 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCu7lK72LkjJuD9FNVI5Agk8sAOLF3BvDw",
+  authDomain: "groove-real-estate-44a4b.firebaseapp.com",
+  projectId: "groove-real-estate-44a4b",
+  storageBucket: "groove-real-estate-44a4b.appspot.com",
+  messagingSenderId: "1051651980073",
+  appId: "1:1051651980073:web:215dcb17aabfe6c6b7d538"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
