7ad192ffe3f031f48eb06c0b5b779549af53209b
5anne <tahaminaanne@gmail.com>
2024-06-08 14:58:45 +0600

    eigth commit

--- src/Home/Contact.jsx
+++ src/Home/Contact.jsx
@@ -1,6 +1,9 @@
 import { Helmet } from "react-helmet-async";
 import Navbar from "../Shared/Navbar";
 import Footer from "../Shared/Footer";
+import { ImAddressBook } from "react-icons/im";
+import { BsFillTelephoneFill } from "react-icons/bs";
+import { MdMarkEmailUnread } from "react-icons/md";
 
 
 const Contact = () => {
@@ -10,6 +13,39 @@ const Contact = () => {
                 <title>Groove Real Estate | Contact</title>
             </Helmet>
             <Navbar></Navbar>
+            <div className="bg-blue-50 p-8 flex justify-center">
+                <div className="bg-white m-14 p-10 w-1/2">
+                    <h1 className="text-4xl font-medium text-center font-display pb-8">Contact Us</h1>
+                    <div className="flex justify-center"><p className="border w-40 border-[#ba8759]"></p></div>
+                    <div className="flex gap-8 mt-8">
+                        <div className="bg-cyan-950 p-4 w-1/2">
+                            <h1 className="font-semibold text-slate-400 font-display text-2xl">Contact Info</h1>
+                            <div className="bg-white rounded-lg p-4 mt-4">
+                                <h2 className="flex gap-2 items-center font-semibold"><ImAddressBook /> Address:</h2>
+                                <p className="text-sm text-gray-600">House # 9/A, Road # 70, Gulshan-01, Dhaka-1212</p>
+                            </div>
+                            <div className="bg-white rounded-lg p-4 mt-4">
+                                <h2 className="flex gap-2 items-center font-semibold"><BsFillTelephoneFill /> Telephone:</h2>
+                                <p className="text-sm text-gray-600">+1 213-718-9077</p>
+                            </div>
+                            <div className="bg-white rounded-lg p-4 mt-4">
+                                <h2 className="flex gap-2 items-center font-semibold"><MdMarkEmailUnread /> Email Us:</h2>
+                                <p className="text-sm text-gray-600">groove_real_estate@gmail.com</p>
+                            </div>
+                        </div>
+                        <div className="h-full">
+                            <h1 className="text-blue-950 font-display text-2xl">Send us a Message</h1>
+                            <form>
+                                <input className="border-2 border-gray-500 rounded-lg p-2 my-2 w-full" type="text" placeholder="Your Name" />
+                                <input className="border-2 border-gray-500 rounded-lg p-2 my-2 w-full" type="email" placeholder="Your Email" />
+                                <input className="border-2 border-gray-500 rounded-lg p-2 my-2 w-full" type="number" placeholder="Your Phone Number" />
+                                <input className="border-2 border-gray-500 rounded-lg p-2 pb-28 my-2 w-full" type="text" placeholder="Write Message"/>
+                            </form>
+                            <button className="bg-cyan-950 px-4 py-2 text-white rounded-md w-full">Send</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer></Footer>
         </div>
     );
--- src/Home/Profile.jsx
+++ src/Home/Profile.jsx
@@ -1,17 +1,92 @@
 import { Helmet } from "react-helmet-async";
 import Navbar from "../Shared/Navbar";
 import Footer from "../Shared/Footer";
+import { useContext, useRef, useState } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { updateProfile } from "firebase/auth";
 
 
 const Profile = () => {
+    const { user } = useContext(AuthContext);
+    const { displayName, photoURL, email } = user;
+    console.log(displayName, photoURL, email);
+    const [active, setActive] = useState(false);
+    const [update, setUpdate] = useState(false);
+    const [name, setName] = useState('');
+    const inputRef = useRef(null);
+
+
+    const handleEdit = e => {
+        e.preventDefault();
+        setActive(true);
+    }
+
+    const handleUpdate = e => {
+        e.preventDefault();
+        const upname = inputRef.current.value;
+        console.log(upname);
+        setUpdate(true);
+        setName(upname);
+    }
+
+    const handleChanges = e => {
+        e.preventDefault();
+        console.log(name);
+        updateProfile(user, {
+            displayName: name
+        })
+            .then(() => {
+                console.log('profile updated')
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
+
     return (
         <div>
             <Helmet>
                 <title>Groove Real Estate | User_Profile</title>
             </Helmet>
             <Navbar></Navbar>
+            <div className="bg-blue-50 md:p-8 flex justify-center">
+                <div className="bg-white my-4 md:m-14 p-8 md:w-1/2">
+                    <h1 className="text-4xl font-medium text-center font-display pb-8">User Login Info</h1>
+                    <div className="flex justify-center"><p className="border w-40 border-[#ba8759]"></p></div>
+                    <div className="flex justify-center my-8">
+                        <img src={photoURL} alt="" />
+                    </div>
+                    <div className="flex justify-center my-8">
+                        <form action="">
+                            <div>
+                                <label className="label">
+                                    <span className="label-text">Name</span>
+                                </label>
+                                <input
+                                    onChange={handleUpdate}
+                                    ref={inputRef}
+                                    className={active ? "bg-gray-200 border-[1px] border-green-800 px-4 py-2" : "bg-gray-200 px-4 py-2"}
+                                    type="text"
+                                    name="name"
+                                    placeholder={displayName}
+                                    disabled={!active} />
+                            </div>
+                            <div>
+                                <label className="label">
+                                    <span className="label-text">Email ID</span>
+                                </label>
+                                <input className="bg-gray-200 px-4 py-2" type="email" placeholder={email} disabled />
+                            </div>
+                            <div className="flex justify-between items-center my-8">
+                                <button onClick={handleEdit} className="btn bg-gray-300">Edit</button>
+                                <button onSubmit={handleChanges} className={update ? "btn bg-gray-500" : "hidden"} > Save the Changes</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div >
             <Footer></Footer>
-        </div>
+        </div >
     );
 };
 
